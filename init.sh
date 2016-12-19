64c2a8f1cc610b97a8167f60736904f6bc6cc24d
Victor Bjelkholm <victorbjelkholm@gmail.com>
2016-12-19 22:03:12 +0100

    js-ipfs is a part of init, add add-and-gc

--- init.sh
+++ init.sh
@@ -3,6 +3,10 @@
 echo "Create Monitoring"
 kubectl create -f ./prometheus-manifests.yml
 
+echo ""
 echo "Create go-ipfs deployment"
 kubectl create -f ./go-ipfs-deployment.yml
 
+echo ""
+echo "Create js-ipfs deployment"
+kubectl create -f ./js-ipfs-deployment.yml
