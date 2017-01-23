dfa8fa1d42f99756f80673a08c2d56e9b9b19772
Jakub Sztandera <kubuxu@protonmail.ch>
2017-01-23 16:24:10 +0100

    Make the RPC work

--- manager/main.go
+++ manager/main.go
@@ -3,17 +3,32 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/rpc"
 	"net/rpc/jsonrpc"
+
+	stump "github.com/whyrusleeping/stump"
 )
 
 var (
-	optPort = flag.Int("port", 9232, "Port")
+	optAddress = flag.String("address", "localhost:9232", "Address to listen or connect to.")
+)
 
 func main() {
-	
+	stump.Verbose = true
+	flag.Parse()
+
+	switch flag.Arg(0) {
+	case "server":
+		log.Printf("Running server at: %s", *optAddress)
+		server()
+	case "client":
+		client(flag.Args()[1:])
+	default:
+		log.Fatalf("Invalid argument: '%s'", flag.Arg(0))
+	}
 }
 
 func server() {
@@ -27,7 +42,7 @@ func server() {
 	}
 
 	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
-	l, err := net.Listen("tcp", ":9232")
+	l, err := net.Listen("tcp", *optAddress)
 	if err != nil {
 		log.Fatal("listen:", err)
 	}
@@ -40,4 +55,43 @@ func server() {
 
 		go server.ServeCodec(jsonrpc.NewServerCodec(conn))
 	}
+}
+
+func client(args []string) {
+	if len(args) == 0 {
+		log.Fatal("not enough arguments")
+	}
+
+	log.Printf("Connecting to RPC server at: %s", *optAddress)
+	conn, err := net.Dial("tcp", *optAddress)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer conn.Close()
+	c := jsonrpc.NewClient(conn)
+
+	empty := struct{}{}
+
+	switch args[0] {
+	case "stop":
+		status := -1
+		err = c.Call("Manager.Stop", empty, &status)
+		defer fmt.Println(status)
+	case "start":
+		startArgs := args[1:]
+		err = c.Call("Manager.Start", startArgs, &empty)
+	case "version":
+		if len(args) != 2 {
+			log.Fatal("wrong args length")
+		}
+		ver := args[1]
+		err = c.Call("Manager.ChangeVersion", ver, &empty)
+	default:
+		log.Fatalf("Unknown function: %s", args[0])
+	}
+
+	if err != nil {
+		log.Fatal("call: ", err)
+	}
+
 }
--- manager/rpc.go
+++ manager/rpc.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"context"
+	"errors"
+	"log"
 	"os"
 	"os/exec"
+	"syscall"
 	"time"
 
 	uplib "github.com/ipfs/ipfs-update/lib"
 	uputil "github.com/ipfs/ipfs-update/util"
+)
+
+var (
+	lg = log.New(os.Stdout, "rpc: ", log.Ldate)
 )
 
 type Manager struct {
@@ -16,7 +23,7 @@ type Manager struct {
 }
 
 func NewManager(ctx context.Context) *Manager {
-	res := &Manager{}
+	res := &Manager{ctx: ctx}
 	return res
 
 }
@@ -37,7 +44,6 @@ func (mn *Manager) Status(_ struct{}, status *RunStatus) error {
 // Removes IPFS_PATH as side effect
 // Forcibly kills ipfs daemon as a side effect
 func (mn *Manager) ChangeVersion(version string, _ *struct{}) error {
-	// TODO: stop existing
 	mn.kill()
 
 	repo := uputil.IpfsDir()
@@ -58,13 +64,17 @@ func (mn *Manager) kill() {
 		return
 	}
 
+	lg.Printf("killing PID: %d", mn.proc.Process.Pid)
+
 	// ignore error
 	_ = mn.proc.Process.Kill()
+	lg.Print("killed")
 	mn.proc = nil
 }
 
 func (mn *Manager) Start(args []string, _ *struct{}) error {
 	mn.kill()
+	lg.Println("starting")
 
 	// ipfs is in path
 	argsAll := make([]string, 1, len(args)+1)
@@ -75,40 +85,51 @@ func (mn *Manager) Start(args []string, _ *struct{}) error {
 	if err != nil {
 		mn.proc = nil
 	}
+	lg.Printf("started 'ipfs daemon' with args %v, PID: %d", args, mn.proc.Process.Pid)
 	return err
 }
 
-type StopStatus int
-
-const (
-	STOP_SOFT StopStatus = iota
-	STOP_DEAD
-	STOP_KILL
-)
-
 // Stops the daemon softly
 // Kills it after 30s
-func (mn *Manager) Stop(_ struct{}, status *StopStatus) error {
+func (mn *Manager) Stop(_ struct{}, status *int) error {
+	if mn.proc == nil {
+		return errors.New("no process is running")
+	}
+
 	ctx, cancel := context.WithTimeout(mn.ctx, 30*time.Second)
 	defer cancel()
 
+	lg.Println("stopping")
 	wait := make(chan error)
 	go func() {
 		wait <- mn.proc.Wait()
 	}()
 
+	lg.Printf("sent SIGINT to PID: %d", mn.proc.Process.Pid)
 	mn.proc.Process.Signal(os.Interrupt)
 	select {
-	case w := <-wait:
-		if w == nil {
-			*status = STOP_SOFT
+	case err := <-wait:
+		if err == nil {
+			lg.Println("stop went fine")
+			*status = 0
 		} else {
-			*status = STOP_DEAD
+			lg.Printf("wait for stop returned error: %s", err)
+			exiterr, ok := err.(*exec.ExitError)
+			if ok {
+				if st, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+					*status = st.ExitStatus()
+					return nil
+				}
+			}
+			lg.Print("could not read exit code from error: ", err)
+			*status = -2
+			return nil
 		}
 		return nil
 	case <-ctx.Done():
+		lg.Print("timeout, killing")
 		mn.kill()
-		*status = STOP_KILL
+		*status = 124
 		return nil
 	}
 }
