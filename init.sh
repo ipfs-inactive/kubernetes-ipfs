3ddf31b184d2a654f678bb09bfec9396add68b55
Frank Petrilli <frank@petril.li>
2017-02-21 12:32:55 -0800

    Remove js-ipfs deployment from init.sh for now
    Since all tests are currently written for go-ipfs,
    no reason to eave a deployment in the init script.
    We'll add it back when js-ipfs tests are added
    
    License: MIT
    Signed-off-by: Frank Petrilli <frank@petril.li>

--- init.sh
+++ init.sh
@@ -8,7 +8,3 @@ kubectl create -f ./prometheus-manifests.yml
 echo ""
 echo "Create go-ipfs deployment"
 kubectl create -f ./go-ipfs-deployment.yml
-
-echo ""
-echo "Create js-ipfs deployment"
-kubectl create -f ./js-ipfs-deployment.yml
