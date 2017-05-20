96bf06f89a7be86da60789ba28dd78fda3a789bf
dgrisham <dgrisham@mines.edu>
2017-05-20 12:12:37 -0600

    fixed typo in init.sh

--- ipfs-cluster/init.sh
+++ ipfs-cluster/init.sh
@@ -45,7 +45,7 @@ set +ex
 echo
 echo "To access Grafana for viewing metrics gathered by Prometheus, run the following commands:"
 echo
-echo $'pod=$kubectl get pods --namespace=monitoring | grep grafana-core | awk \'{print $1}\''
+echo $'pod=$(kubectl get pods --namespace=monitoring | grep grafana-core | awk \'{print $1}\')'
 echo 'kubectl port-forward --namespace=monitoring $pod 3000:3000'
 echo 
-echo 'Then navigate to localhost:3000 in your browser.'
+echo "Then navigate to localhost:3000 in your browser."
