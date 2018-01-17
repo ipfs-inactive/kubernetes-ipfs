dfca91485dafcdb088184e50b42a5cf44154fae2
Frank Petrilli <frank@petril.li>
2018-01-17 09:47:13 -0800

    Working toward #42

--- ipfs-cluster/trigger-server/triggerserver.go
+++ ipfs-cluster/trigger-server/triggerserver.go
@@ -14,13 +14,13 @@ import (
     "bytes"
 )
 
-const CONFIG_PATH = "/etc/trigger-server"
-// HTTP API listen port
-const PORT = 8082
-var RUNNER_PATH = os.Getenv("GOPATH") + "src/github.com/ipfs/kubernetes-ipfs/ipfs-cluster/"
+const configPath = "/etc/trigger-server"
+// port is HTTP API listen port
+const port = 8082
+var runnerPath = os.Getenv("GOPATH") + "src/github.com/ipfs/kubernetes-ipfs/ipfs-cluster/"
 // Runner takes two args
-const RUNNER_NUM_NODES = "5"
-const RUNNER_NUM_PINS = "5"
+const runnerNumNodes = "5"
+const runnerNumPins = "5"
 
 // Secret is set by main
 var secret = ""
@@ -35,8 +35,8 @@ func run(rw http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(rw, "Invalid authentication\n")
 	} else {
 		// This command takes a while, so we'll hang out here while it runs
-		cmd := exec.Command("bash", "runner.sh", RUNNER_NUM_NODES, RUNNER_NUM_PINS)
-		cmd.Dir = RUNNER_PATH
+		cmd := exec.Command("bash", "runner.sh", runnerNumNodes, runnerNumPins)
+		cmd.Dir = runnerPath
 		out, err := cmd.CombinedOutput()
 		// Add the output to IPFS
 		addr, ipfsErr := sh.Add(bytes.NewReader(out))
@@ -63,25 +63,25 @@ func run(rw http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-    secret_bytes, readErr := ioutil.ReadFile(CONFIG_PATH)
+    secretBytes, readErr := ioutil.ReadFile(configPath)
     if readErr != nil {
-        log.Printf("Missing secret configuration in %s. We'll generate one for you...\n", CONFIG_PATH)
+        log.Printf("Missing secret configuration in %s. We'll generate one for you...\n", configPath)
 	rand.Seed(time.Now().UnixNano())
-	secret_bytes = randLetters(30)
-	ioErr := ioutil.WriteFile(CONFIG_PATH, secret_bytes, 0644)
+	secretBytes = randLetters(30)
+	ioErr := ioutil.WriteFile(configPath, secretBytes, 0644)
 	if ioErr != nil {
 		log.Fatal("Unable to write new secret file; check permissions and try again")
 	}
     }
-    secret = string(secret_bytes)
-    log.Printf("Starting with secret as %s on port %d\n", secret, PORT)
+    secret = string(secretBytes)
+    log.Printf("Starting with secret as %s on port %d\n", secret, port)
     http.HandleFunc("/run", run)
     http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
         fmt.Fprintf(rw, `Kubernetes-IPFS Trigger Server
 	https://github.com/ipfs/kubernetes-ipfs/
 	`)
     })
-    log.Println(http.ListenAndServe(":" + strconv.FormatInt(PORT, 10), nil))
+    log.Println(http.ListenAndServe(":" + strconv.FormatInt(port, 10), nil))
 }
 
 var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
